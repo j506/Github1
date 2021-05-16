909ff9c64ffcd00f94a1733b33e15d03bd909fbc
jagannath moharana <jagannathmoharana96@gmail.com>
2021-05-16 22:04:46 +0530

    second_commit

--- src/test/java/com/testcase/LoginTest.java
+++ src/test/java/com/testcase/LoginTest.java
@@ -1,5 +1,13 @@
 package com.testcase;
 
 public class LoginTest {
+	public void loginPage()
+	{
+		System.out.println("Login fuction reposiratory page");
+	}
+	public void info()
+	{
+		System.out.println("hyguu");
+	}
 
 }
